04785aaeec6a9a68b00ed16f5cbe991eed31c74b
Jordan Abderrachid <jabderrachid@gmail.com>
2014-12-24 17:33:37 +0100

    Add main controller.

---
+++ public/js/controller/mainCtrl.js
@@ -0,0 +1,10 @@
+chatApp.controller('mainCtrl', function ($scope) {
+	$scope.messages = [
+		'Lorem ipsum dolor.',
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+		'Lorem ipsum.',
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae!',
+		'Lorem ipsum dolor sit amet.',
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, veritatis!'
+	];
+});
